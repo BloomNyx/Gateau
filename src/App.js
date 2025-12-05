5154128bc23dd48f0a9b0fcdcf2f18e443312756
BloomNyx <bagsuhyeon507@gmail.com>
2025-12-05 09:24:16 +0900

    gateau

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import HomeView from "./views/HomeView";
 
 const App = () => {
     return (
-    <BrowserRouter>
+    <BrowserRouter basename="Gateau">
         <Routes>
             <Route path="/" element={<HomeView />} />
         </Routes>
